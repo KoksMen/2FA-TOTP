2ea81a92ecd7f5c8de51e07bde0294ff2a3a6cdb
xXKoksMenXx <105588059+KoksMen@users.noreply.github.com>
2023-03-27 21:27:00 +0300

    Update AssemblyInfo.cs

--- Examples/Properties/AssemblyInfo.cs
+++ Examples/Properties/AssemblyInfo.cs
@@ -5,14 +5,14 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("Examples")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("2FA-TOTP Koks Inc.")]
+[assembly: AssemblyDescription("2FA-TOTP - Convenient use OTP.Net")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Koks Inc.")]
 [assembly: AssemblyProduct("Examples")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTrademark("Koks Inc.")]
+[assembly: AssemblyCulture("RU-EN")]
 
 // Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
 // для компонентов COM. Если необходимо обратиться к типу в этой сборке через
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номера сборки и редакции по умолчанию 
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.5.0.2")]
+[assembly: AssemblyFileVersion("1.3.0.2")]
